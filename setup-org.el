2fd33d9dcaf75d7605de49ee7cd39d159b05ae45
zjroth <zjroth@gmail.com>
2014-05-22 10:57:07 -0500

    Tweak org-mode settings

--- setup-org.el
+++ setup-org.el
@@ -10,4 +10,10 @@
 (defadvice kill-whole-line (after fix-cookies activate)
   (myorg-update-parent-cookie))
 
+;; Indent headlines and content.
+(setq org-startup-indented t)
+
+;; I use this command too much outside of org-mode to not have it here, too.
+(define-key org-mode-map (kbd "<C-S-return>") 'open-line-above)
+
 (provide 'setup-org)
