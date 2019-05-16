5e2a65ec3a2f5ae5a292799cbd4e856ec0469d3a
zach.roth@mutualofomaha.com <zach.roth@mutualofomaha.com>
2019-05-16 15:39:15 -0500

    Updates to configuration of ivy
    - Both counsel and swiper are loaded along with ivy; don't load them explicitly.
    - The display transformer wasn't working with ivy-rich; remove it for now.
    - Bind keys for org-mode-map with a separat `:bind` clause.

--- settings/use-ivy.el
+++ settings/use-ivy.el
@@ -9,14 +9,18 @@
   :config
   (progn
     (ivy-mode 1)
-    ;; add ‘recentf-mode’ and bookmarks to ‘ivy-switch-buffer’.
     (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t)
+
     ;; number of result lines to display
     (setq ivy-height 10)
+
     ;; Display the candidate count and the current candidate.
     (setq ivy-count-format "(%d of %d) ")
+
     ;; no regexp by default
     (setq ivy-initial-inputs-alist nil)
+
     ;; configure regexp engine.
     (setq ivy-re-builders-alist
           ;; allow input not in order
@@ -36,34 +40,38 @@
        cands
        "\n"))
 
+    ;; How to separate org headlines when displaying full paths with ivy.
+    (setq counsel-org-headline-path-separator " ▶ ")
+
     (setq ivy-format-function 'ivy-format-function-arrow)
-    (setq ivy-use-selectable-prompt t)
-
-    (use-package counsel
-      :config
-      (setq counsel-org-headline-path-separator " ▶ "))
-
-    (use-package swiper)
-
-    (use-package ivy-rich
-      :init
-      (ivy-set-display-transformer 'ivy-switch-buffer
-                                   'ivy-rich-switch-buffer-transformer)))
+    (setq ivy-use-selectable-prompt t))
 
   :bind (("C-x b"     . ivy-switch-buffer)
          ("M-y"       . counsel-yank-pop)
          ("M-x"       . counsel-M-x)
          ("C-s"       . swiper)
          ("C-x C-f"   . counsel-find-file)
-         ("C-c C-o"   . ivy-occur)
+         ("C-c C-o"   . ivy-occur))
 
-         :map org-mode-map
-         ("C-c C-j"   . counsel-org-goto-all)
-         ;("C-h a"     . helm-apropos)
-         ;("C-x b"     . helm-buffers-list)
-         ;("C-x c o"   . helm-occur)
-         ;("C-x c SPC" . helm-all-mark-rings)
-         )
+  :bind (:map org-mode-map
+              ("C-c C-j"   . counsel-org-goto-all)
+              ;; ("C-h a"     . helm-apropos)
+              ;; ("C-x b"     . helm-buffers-list)
+              ;; ("C-x c o"   . helm-occur)
+              ;; ("C-x c SPC" . helm-all-mark-rings)
+              )
+  )
+
+(use-package ivy-rich
+  :after (:all ivy counsel)
+
+  :init
+  (setq ivy-rich-parse-remote-file-path t)
+  ;; (ivy-set-display-transformer 'ivy-switch-buffer
+  ;;                              'ivy-rich-switch-buffer-transformer)
+
+  :config
+  (ivy-rich-mode 1)
   )
 
 (provide 'use-ivy)
