355dc4c4b19206893d87fffeda193f831dbf33d8
zach.roth@mutualofomaha.com <zach.roth@mutualofomaha.com>
2019-06-19 08:02:04 -0500

    Change some mac-related settings

--- settings/mac.el
+++ settings/mac.el
@@ -20,15 +20,9 @@
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (push "/usr/local/bin" exec-path)
 
-;; keybinding to toggle full screen mode
-(global-set-key (quote [M-f10]) (quote ns-toggle-fullscreen))
-
 ;; Move to trash when deleting stuff
 (setq delete-by-moving-to-trash t
       trash-directory "~/.Trash/emacs")
-
-;; Ignore .DS_Store files with ido mode
-(add-to-list 'ido-ignore-files "\\.DS_Store")
 
 ;; Don't open files from the workspace in a new frame
 (setq ns-pop-up-frames nil)
