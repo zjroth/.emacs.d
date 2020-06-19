0fed050a6d06400c9b652e8aec38d2d56f5dcc91
zach.roth@mutualofomaha.com <zach.roth@mutualofomaha.com>
2020-06-19 11:54:11 -0500

    Updates to python experience

--- settings/use-python.el
+++ settings/use-python.el
@@ -1,6 +1,10 @@
 
 (use-package realgud)
 
+(add-hook 'python-mode-hook
+          (lambda ()
+            (set-fill-column 79)))
+
 ;; An IDE-like experience
 (use-package elpy
   :config
@@ -12,6 +16,8 @@
   (setq python-shell-interpreter "jupyter"
         python-shell-interpreter-args "console --simple-prompt --kernel python3"
         python-shell-prompt-detect-failure-warning nil)
+
+  (setq elpy-rpc-virtualenv-path 'current)
 
   (add-to-list 'python-shell-completion-native-disabled-interpreters
                "jupyter")
