7ce5f398fbaa3be8f91d01f20a2594e727586176
Zach Roth <zach@crescent.app>
2022-01-26 10:43:07 -0600

    Use display-line-numbers-mode in goto-line-with-feedback
    It seems that linum-mode isn't working for me.  It's not causing an error; it's
    just not displaying any line numbers.

--- defuns/misc-defuns.el
+++ defuns/misc-defuns.el
@@ -20,12 +20,14 @@
 (defun goto-line-with-feedback ()
   "Show line numbers temporarily, while prompting for the line number input"
   (interactive)
-  (let ((linum-state (if (boundp 'linum-mode) linum-mode nil)))
+  (let ((curr-state (if (boundp 'display-line-numbers-mode)
+                        display-line-numbers-mode
+                      nil)))
     (unwind-protect
          (progn
-           (linum-mode 1)
+           (display-line-numbers-mode 1)
            (call-interactively 'goto-line))
-      (linum-mode (if linum-state 1 -1)))))
+      (display-line-numbers-mode (if curr-state 1 -1)))))
 
 ;; Add spaces and proper formatting to linum-mode. It uses more room than
 ;; necessary, but that's not a problem since it's only in use when going to
